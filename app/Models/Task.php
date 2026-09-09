d5940327f8ec6994fec4c8ac0fd6008d68986b85
tareks-2026 <tarek.saleh@profil-hannover.de>
2026-09-09 10:35:53 +0200

    migration user_id in tasks

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    protected $fillable = ['title', 'description', 'done'];
+    protected $fillable = ['title', 'description', 'done', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
---
+++ database/migrations/2026_09_09_081854_add_user_id_to_tasks_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->foreignId('user_id')
+            ->nullable() // Da wir bereits task ohne User id haben würde es zu einem fehler kommen ohne nullable
+            ->constrained()
+            ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            //
+        });
+    }
+};
