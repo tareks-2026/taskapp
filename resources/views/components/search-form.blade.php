7672a5fac3f61a1f0ccfb06046eca62693bc9d1e
tareks-2026 <tarek.saleh@profil-hannover.de>
2026-09-10 12:06:05 +0200

    alle anzeigen button

--- resources/views/components/search-form.blade.php
+++ resources/views/components/search-form.blade.php
@@ -7,4 +7,5 @@
     <input type="text" name="q" value="{{ request('q') }}"
         placeholder="{{ $placeholder }}" class="input w-full max-w-md">
     <button type="submit" class="btn btn-primary">Suchen</button>
+    <a href="{{ route($route) }}" class="btn shrink-0">Alle Anzeigen</a>
 </form>
